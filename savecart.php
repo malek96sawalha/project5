a939e37f928319016adcd9e750065adef8088422
malek96sawalha <maleksawalha1996@gmail.com>
2023-08-21 00:00:13 +0300

    m

---
+++ savecart.php
@@ -0,0 +1,47 @@
+<?php
+include 'includes/head-vars.php';
+
+if (isset($_SESSION['loggedInStatus']) && $_SESSION['loggedInStatus'] == true) {
+    $loggedInId = $_SESSION['loggedInUserData']['id'];
+    $array_product = $_SESSION['cart'];
+
+    // Loop through the cart array and insert items into the cart table if not already inserted
+    foreach ($_SESSION['cart'] as $item) {
+        echo $product_id = $item['productid'];
+        echo $product_quantity = $item['quantity'];
+        // echo '<pre>';
+        // var_dump($_SESSION);
+        // echo '</pre>';
+        // var_dump($_SESSION);
+        
+        // Check if the item is already in the cart table
+        $check_sql = "SELECT * FROM cart WHERE user_id='$loggedInId' AND product_id = '$product_id'";
+        $check_result = mysqli_query($conn, $check_sql);
+
+        if (mysqli_num_rows($check_result) == 0) {
+            // Insert the item into the cart table
+            $sql_c = "INSERT INTO cart (user_id, product_id, product_quantity)
+                      VALUES ('$loggedInId', '$product_id', '$product_quantity')";
+            $result = mysqli_query($conn, $sql_c);
+
+            if ($result) {
+                // echo "Inserted successfully";
+                header('Location: checkout.php');
+            } else {
+                echo "Error: " . mysqli_error($conn);
+                // header('Location: shopping-cart.php');
+            }
+        }
+    }
+
+    // Clear the cart in the session after insertion
+    //$_SESSION['cart'] = array();
+    unset($_SESSION['cart'] );
+
+    header('Location: checkout.php');
+    exit(0);
+} else {
+    $loggedInId = NULL;
+    // header('Location: shopping-cart.php');
+}
+?>
